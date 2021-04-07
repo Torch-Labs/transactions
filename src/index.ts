756f9988a1f86058601900ef4b6e129d0610a524
Sharith Godamanna <sharithg@Shariths-MacBook-Pro.local>
2021-04-06 23:20:25 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -12,29 +12,33 @@ function getProxyFee(plan_name: SellingPlans) {
   return PROXY_FEES[plan_name];
 }
 
-function handleSale(plan_name: SellingPlans, paid: string) {
+function handleSale(plan_name: SellingPlans, paid: string, count: string) {
   return (
-    parseFloat(paid) * STRIPE_PERCENTAGE - STRIPE_FEE - getProxyFee(plan_name)
+    parseFloat(paid) * STRIPE_PERCENTAGE -
+    STRIPE_FEE -
+    getProxyFee(plan_name) * parseFloat(count)
   );
 }
 
 function handleGiveaway(
   plan_name: GwPlans,
   paid_status: PaidStatus,
-  price: string
+  price: string,
+  count: string
 ) {
   if (plan_name === OLD_GIVEAWAY) return 0;
-  if (paid_status === CREDIT) return parseFloat(price) * -1;
-  return parseFloat(price);
+  if (paid_status === CREDIT) return parseFloat(price) * parseFloat(count) * -1;
+  return parseFloat(price) * parseFloat(count);
 }
 
 export function calcTransFees(
   plan_name: GwPlans & SellingPlans,
   paid: string,
   paid_status: PaidStatus,
-  price: string
+  price: string,
+  count: string
 ) {
   if (GW_PRICES[plan_name])
-    return handleGiveaway(plan_name, paid_status, price);
-  return handleSale(plan_name, paid);
+    return handleGiveaway(plan_name, paid_status, price, count);
+  return handleSale(plan_name, paid, count);
 }
