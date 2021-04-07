d0091dfec656a703ebeccd6abcb3ccac443a76c1
Sharith Godamanna <sharithg@Shariths-MacBook-Pro.local>
2021-04-06 23:26:00 -0400

    update calc trans

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
 import { SellingPlans, GwPlans, PaidStatus } from "./types";
-export declare function calcTransFees(plan_name: GwPlans & SellingPlans, paid: string, paid_status: PaidStatus, price: string): number;
+export declare function calcTransFees(plan_name: GwPlans & SellingPlans, paid: string, paid_status: PaidStatus, price: string, count: string): number;
 //# sourceMappingURL=index.d.ts.map
